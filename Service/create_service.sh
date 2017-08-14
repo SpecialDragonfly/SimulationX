585df66d81b7366abc303a9debd5d388873fd84f
Dominic Orme <Dominic.Orme@worldfirst.com>
2017-08-14 16:08:15 +0100

    Adds example files for .env and .sxpayload

--- Service/create_service.sh
+++ Service/create_service.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cp .sxpayload.example ./Api/resources/.sxpayload
-docker-compose up -d
 docker-compose exec php_apache -c "chmod -R 777 /var/www"/storage/logs"
 docker-compose exec php_apache -c "touch /var/www/database/resources.json" && chmod 777 /var/www/database/resources.json"
-#docker-compose exec php_apache -c "cd Api && php artisan create:service $1 $2"
+docker-compose exec php_apache -c "cd Api && php artisan create:service $1 $2 $3"
